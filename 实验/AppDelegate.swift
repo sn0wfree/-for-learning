42f5e5fa36502b98b1fd2858612cfd18c7b3bc45
sn0w2free <linlu1234567@sina.com>
2014-06-09 17:17:37 +0800

    Initial Commit

---
+++ 实验/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  实验
+//
+//  Created by linlu on 14-6-9.
+//  Copyright (c) 2014年 linlu. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 实验/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  实验
+//
+//  Created by linlu on 14-6-9.
+//  Copyright (c) 2014年 linlu. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
